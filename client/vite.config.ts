fc2011f86c3fe9ebf63efc13b9a2d2bd7b0d12d4
Berta Balázs <95berta.balazs@gmail.com>
2025-01-07 20:47:16 +0100

    GH pages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/", // Used for GH Pages
+  base: "/portfolio", // Used for GH Pages
   plugins: [react()],
   build: {
     rollupOptions: {
